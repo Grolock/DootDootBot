cb2b52d688c0ddef8dc5c76f67b820d6470be414
Josh Martin <Grollock11@gmail.com>
2019-10-01 09:03:02 -0400

    ascii test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const anilist = require('anilist-node');
 const Anilist = new anilist();
 // Load up the discord.js library
 const Discord = require("discord.js");
-const Ascii = require('ascii');
+var Ascii = require('ascii');
 
 // This is your client. Some people call it `bot`, some people call it `self`,
 // some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
