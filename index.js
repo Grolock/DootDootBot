7e53c630d4d8d6091d93fca5d95eaaeb5396eee0
Josh Martin <Grollock11@gmail.com>
2020-03-08 00:44:53 -0500

    Any anime update

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ client.on("message", async message => {
   };
 
   if(message.content.toLowerCase().includes('when does') && message.content.toLowerCase().includes('come out')) {
-    let id = message.substring(11, 16)
+    let id = message.content.substring(11, 16)
     console.log(id)
     getAnime(id, message)
   }
