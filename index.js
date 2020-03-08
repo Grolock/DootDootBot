2370a834ba7495a60c0db6e99c0414ee29503b07
Josh Martin <Grollock11@gmail.com>
2020-03-08 01:36:43 -0500

    Any anime update

--- index.js
+++ index.js
@@ -150,7 +150,7 @@ client.on("message", async message => {
 
 function getAnimeByName(searchTerm, message) {
   Anilist.search('anime', searchTerm, 1, 1).then(data => {
-    message.channel.send(`${data.media.title.english} Will release in `)
+    console.log(data.media.title.english)
     getAnime(data.media.id, message)
   });
 }
